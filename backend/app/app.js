a5c02df76a3fc71f29a92b6aa42f1e6abbc33406
Vihermaa <janne.vihermaa@gmail.com>
2023-12-03 22:58:58 +0200

    Answer middleware questions

--- backend/app/app.js
+++ backend/app/app.js
@@ -7,10 +7,15 @@ import routes from './routes.js'
 const app = express()
 
 // MB-TODO: What are middlewares in Express?
+// Functions/operations to perform between receiving the request and returning the response, like modify the request/response
 // MB-TODO: What these middlewares do?
+// Parse incoming requests with JSON payload and put the parsed data to req.body
 app.use(express.json())
+// Parse incoming requests with urlencoded payloads. Extended allows parsing more complex data structures
 app.use(express.urlencoded({ extended: true }))
+// Set HTTP headers to improve security
 app.use(helmet())
+// Set logging using minimal format
 app.use(morgan('tiny'))
 
 app.use(routes)
