54b9bf8e01bfe4cf609c5f13f52edc50daa57e04
Vihermaa <janne.vihermaa@gmail.com>
2023-12-03 22:36:40 +0200

    Add status message to show that adding contacts actually does something

--- frontend/src/components/NewCustomerContact.jsx
+++ frontend/src/components/NewCustomerContact.jsx
@@ -1,22 +1,33 @@
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { createCustomerContact } from '../customerSlices'
 import PropTypes from 'prop-types';
 
 const NewCustomerContact = ({ customerId, contacts}) => {
   const dispatch = useDispatch()
+  const [showSuccess, setShowSuccess] = useState(false);
 
   // Add random contact to customer
-  const createNewCustomerContact = () => {
+  const createNewCustomerContact = async () => {
     const contactId = contacts[Math.floor(Math.random() * contacts.length) + 1].id;
-    dispatch(createCustomerContact({customerId, contactId}))
+    const response = await dispatch(createCustomerContact({customerId, contactId}))
+
+    if (response.meta.requestStatus === 'fulfilled') {
+      setShowSuccess(true)
+      setTimeout(() => {setShowSuccess(false)}, 5000)
+    }
   }
 
   return (
+    <>
+      {showSuccess ? <div className="fixed-top text-center text-bg-success">Saved, see database.json because contact fetching is broken</div> : null}
+      <h1 className='fw-bold'>Customer</h1>
     <button className='btn btn-outline-primary' onClick={createNewCustomerContact}>
       <i className="bi bi-plus" />
       {' '}
       Add new random contact
     </button>
+    </>
   )
 }
 
